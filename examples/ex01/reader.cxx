c999f429bf89a903cecee2ddaca3a06eb304e47e
batkov <batkov@70b30a08-bd64-11dd-824f-6f77523b18d3>
2008-11-30 17:21:44 +0000

    reduced number of bins in ex01

--- examples/ex01/reader.cxx
+++ examples/ex01/reader.cxx
@@ -8,8 +8,9 @@ int main(int argc, const char **argv)
 	ReadFluka::Reader::gVerbose = ReadFluka::kPRINT_HEADER;
 	ReadFluka::UsrBin *usrbin = new ReadFluka::UsrBin(argv[1]);
 
+	unsigned int nbins = 0;
 	while(usrbin->Read()) {
-		cout << "here" << endl;
+		usrbin->Print();
 	}
 	delete usrbin;
 
