2d6f9cf863339430a3ea56525b3ea37384352d46
batkov <batkov@gmail.com>
2010-10-15 11:28:34 +0000

    usbsuw2txt added

---
+++ tools/src/usbsuw2txt.cxx
@@ -0,0 +1,24 @@
+#include "UsbSuw.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+  ReadFluka::Base::gVerbose = ReadFluka::kPRINT_NOTHING;
+  ReadFluka::UsbSuw *usbsuw = new ReadFluka::UsbSuw(argv[1]);
+  
+  /*cout << "***** " << usbsuw->GetRunTitle() << " *****" << endl;
+  cout << usbsuw->GetRunTime() << endl;
+  cout << "Total number of particles followed " << usbsuw->GetEntryNumber();
+  cout << ", for a total weight of " << usbsuw->GetWeight() << endl;
+  */
+
+  while (usbsuw->Read()) {
+    usbsuw->Print();
+  }
+
+  delete usbsuw;
+ 
+  return 0;
+}
