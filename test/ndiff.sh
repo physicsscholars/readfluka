cf8e09f5c98f50deffbb2777e027d286f95eb31d
batkov <batkov@70b30a08-bd64-11dd-824f-6f77523b18d3>
2010-11-16 11:16:48 +0000

    test: ndiff.sh and usxsuw.sh implemented

--- test/ndiff.sh
+++ test/ndiff.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 # A wrapper script to compare 2 text files, ignoring small numeric differences
 # Needs ndiff ( http://www.math.utah.edu/~beebe/software/ndiff )
+#
+# Usage: ndiff.sh TESTID file1 file2
+#   or   ndiff.sh TESTID <(cat file1) <(cat file2)
+#        TESTID - any string to describe the current test
 
 RELERR=1.0E-5
-ABSERR=1.0E-5
+ABSERR=1.0E-4
 
-ndiff -abserr $ABSERR --relative-error $RELERR <(grep -v "^\ *$" $1) <(grep -v "^\ *$" $2) && echo -e "GOOD:\t" \"$1\" and \"$2\" are the same with max possible relative error $RELERR and absolute error $ABSERR
+TESTID=$1
+
+# ^\ *$ - removes empty lines or lines with spaces only
+# Terminal display attributes (set color): http://www.termsys.demon.co.uk/vtansi.htm
+ndiff -abserr $ABSERR --relative-error $RELERR <(grep -v "^\ *$" $2) <(grep -v "^\ *$" $3) && echo -e "\033[32m"$TESTID" TEST PASSED\033[0m" || echo -e "\033[31m"$TESTID" TEST FAILED\033[0m"
---
+++ test/usxsuw.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+TMP=$(mktemp)
+OUT=test_usxsuw
+
+for file in *usrbdx; do echo $file >> $TMP; done
+
+echo "" >> $TMP
+echo  $OUT >> $TMP
+
+cat $TMP | $FLUTIL/usxsuw 2>/dev/null
+
+#usxsuw2txt $OUT > a.txt
+
+#bash ndiff.sh a.txt "$OUT"_sum.lis
+
+bash ndiff.sh USXSUW <(usxsuw2txt $OUT) <(cat "$OUT"_sum.lis)
+
+rm -f $TMP
