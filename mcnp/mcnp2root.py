1314d5543a2f39f467cc77843e26fb416e417954
batkov <batkov@gmail.com>
2011-02-02 16:31:44 +0000

    time bins read

--- mcnp/mcnp2root.py
+++ mcnp/mcnp2root.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python
-# Plots two histograms with average and maximum CPU time used to follow a projectile in FLUKA
 
 import sys,time,os,re
 from ROOT import ROOT, TH1F, TFile
@@ -58,12 +57,134 @@ def GetHistogram(fname):
     h.SetEntries(nentries)
     return h
 
+
+class Tally():
+    """
+    A class for tally
+    """
+    data = None
+    number = None
+    type = None
+    particles = None
+    printing_order = None
+    surfaces = None
+    time_bins = []
+    
+    def __init__(self, data, number):
+        self.number = number
+
+        is_tally_found = False
+        is_time_bins_found = False
+
+        iline = 1
+        line_start = None
+        line_end = None
+        for line in data:
+            if re.search("\A1tally  %d" % number, line):
+                is_tally_found = True
+                line_start = iline
+                print "found tally %d" % number
+                print "line start", line_start
+
+            if is_tally_found and iline>line_start and re.search("1tally", line):
+                line_end = iline-1
+                print "line end:", line_end
+                break
+            iline = iline+1
+
+        if not is_tally_found:
+            print "tally %d not found" % number
+            return
+
+        for line in data[line_start:line_end]: # lines with the current tally only
+            if self.type is None:
+                if re.search("tally type", line):
+                    self.type = line.split()[2]
+                    print "\ttype: ", self.type
+
+            if self.particles is None:
+                if re.search('particle\(s\)', line):
+                    self.particles = line.split()[1:]
+                    print "\tparticles: ", self.particles
+                    
+            if self.printing_order is None:
+                if re.search("order of printing:", line):
+                    self.printing_order = line.split()[3:]
+                    print self.printing_order
+
+            if self.surfaces is None:
+                if re.search("surfaces", line):
+                    self.surfaces = line.split()[1:]
+                    print self.surfaces
+
+            if len(self.time_bins) is 0:
+                if re.search("time  bins", line):
+                    is_time_bins_found = True
+                
+            if is_time_bins_found:
+                if re.search(" to ", line):
+                    words = line.split()
+                    self.time_bins.append(words[0])
+                    self.time_bins.append(words[2])
+        print self.time_bins
+
+    def GetTimeBins(self, tally):
+        """
+        Return the time bins [shakes]
+        """
+        for line in self.mcnp_file_contents:
+            if re.search("1tally  %d" % tally, line):
+                if re.search("  time  bins", line):
+                    print line
+
+
+
+class Converter():
+    """
+    A class to convert the MCNP output to the ROOT format
+    """
+    mcnp_fname = None
+    mcnp_file  = None
+    mcnp_file_contents = [] # contents of mcnp_file
+
+    root_fname = None
+    
+    def __init__(self, mcnp_fname, root_fname="out.root"):
+        self.mcnp_fname = mcnp_fname
+        self.root_fname = root_fname
+        self.mcnp_file  = open(mcnp_fname)
+        self.mcnp_file_contents = self.mcnp_file.readlines()
+
+    def GetMCNPVersion(self):
+        """
+        Return the MCNP version as reported in the output file
+        """
+        return self.mcnp_file_contents[0].split()[2]
+
+    def GetInputFileName(self):
+        """
+        Return the input file name
+        """
+        return self.mcnp_file_contents[2]
+
+    def GetTally(self, N):
+        """
+        Return a tally number N
+        """
+        return Tally(self.mcnp_file_contents, N)
+
 def main():
     fname_in = sys.argv[1]
     fname_out = fname_in.replace(".txt", ".root")
-    fout = TFile(fname_out, "RECREATE")
-    GetHistogram(fname_in).Write()
-    fout.Close()
+
+    conv = Converter(fname_in, fname_out)
+    print "MCNP version: ", conv.GetMCNPVersion()
+    print "Input file: ", conv.GetInputFileName()
+    conv.GetTally(11)
+
+#    fout = TFile(fname_out, "RECREATE")
+#    GetHistogram(fname_in).Write()
+#    fout.Close()
     
 
 
